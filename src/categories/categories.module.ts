9444bd483d00504a0ac7df2c791b5841acc882a5
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-07-13 20:03:00 -0300

    fix: fixed wrong controller bug

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 
 @Module({
 	imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
 	controllers: [CategoriesController],
-	providers: [CategoriesService, JwtStrategy],
+	providers: [CategoriesService],
 })
 export class CategoriesModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,10 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
 	imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
 	controllers: [ProductsController],
-	providers: [ProductsService, JwtStrategy],
+	providers: [ProductsService],
 })
 export class ProductsModule {}
