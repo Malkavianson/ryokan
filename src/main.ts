c8ae2c9fbb86b796f2f2cd018c7b287013581f46
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-07-13 22:34:10 -0300

    feature: adjust to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
@@ -8,18 +9,23 @@ const PORT = process.env.PORT || 3333;
 async function bootstrap() {
 	console.clear();
 	console.log('Starting and validating');
-	const app = await NestFactory.create(AppModule);
+
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+
+	app.set('trust proxy', 1);
+
 	app.useGlobalPipes(new ValidationPipe());
+
 	console.log('Server Started\n\nMapping documentation');
 
 	const config = new DocumentBuilder().setTitle('Ryokan').setDescription('API de controle de Ryokan').setVersion('1.1').addTag('Status').addTag('Users').addTag('Tables').addTag('Products').addTag('Categories').addBearerAuth().addServer('http://localhost:3333').build();
 
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('docs', app, document);
-	console.log('Swagger.setup Builded');
 
-	app.enableCors();
-	console.log('CORS Enabled\n\nMapping routes:');
+	console.log('Swagger.setup Builded');
+	console.log('Mapping routes:');
+
 	await app.listen(PORT, () => console.log(`App bootstraped at http://localhost:${PORT}`));
 }
 bootstrap();
